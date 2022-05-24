2caa1d67fca7c657e39774ea9bd82d64b9d71624
darry <792478594@qq.com>
2022-05-24 18:02:13 +0800

    '222'

--- pages/detailed.tsx
+++ pages/detailed.tsx
@@ -1,56 +1,46 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Row, Col, Breadcrumb } from "antd";
+import { Row, Col, Breadcrumb, Affix } from "antd";
 import {
   CalendarOutlined,
   FolderOutlined,
   FireOutlined,
 } from "@ant-design/icons";
-import ReactMarkdown from "react-markdown";
+import axios from "axios";
+import { marked } from "marked";
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
 
+import Tocify from "../components/tocift";
 import Header from "../components/Header";
 import Author from "../components/Author";
 import Advert from "../components/Advert";
 import Footer from "../components/Footer";
 import styles from "../styles/pages/detailed.module.css";
 
-const Detailed: NextPage = () => {
-  let markdown =
-    "# P01:课程介绍和环境搭建\n" +
-    "[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n" +
-    "> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n" +
-    "**这是加粗的文字**\n\n" +
-    "*这是倾斜的文字*`\n\n" +
-    "***这是斜体加粗的文字***\n\n" +
-    "~~这是加删除线的文字~~ \n\n" +
-    "`console.log(111)` \n\n" +
-    "# p02:来个Hello World 初始Vue3.0\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n" +
-    "***\n\n\n" +
-    "# p03:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p04:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "#5 p05:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p06:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "# p07:Vue3.0基础知识讲解\n" +
-    "> aaaaaaaaa\n" +
-    ">> bbbbbbbbb\n" +
-    ">>> cccccccccc\n\n" +
-    "``` var a=11; ```";
+const Detailed: NextPage = (props) => {
+  const tocify = new Tocify();
+  const renderer = new marked.Renderer();
+
+  renderer.heading = function (text, level, raw) {
+    const anchor = tocify.add(text, level);
+    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  };
+
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    breaks: false,
+    smartLists: true,
+    highlight: function (code: any) {
+      return hljs.highlightAuto(code).value;
+    },
+  });
+
+  let html = marked(props.articleContent.article_content);
 
   return (
     <div>
@@ -96,19 +86,34 @@ const Detailed: NextPage = () => {
                 520人
               </span>
             </div>
-            <div className={styles.detailedContent}>
-              <ReactMarkdown sourcePos={false}>{markdown}</ReactMarkdown>
-            </div>
+            <div
+              className={styles.detailedContent}
+              dangerouslySetInnerHTML={{ __html: html }}
+            ></div>
           </div>
         </Col>
         <Col className={styles.commRight} xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
           <Advert />
+          <Affix offsetTop={5}>
+            <div className={styles.navTitle}>文章目录</div>
+            {tocify && tocify.render()}
+          </Affix>
         </Col>
       </Row>
       <Footer />
     </div>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const id = context.query.id;
+  const res = await axios(`http://127.0.0.1:7001/default/getArticleById/${id}`);
+  const articleContent = res.data.data[0];
+  console.log(articleContent);
+  return {
+    props: { articleContent },
+  };
+}
 
 export default Detailed;
